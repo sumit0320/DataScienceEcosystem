69dc73bc32f56b8d539211c5d1bb7866612e2f86
SUMIT ANAND DAS <137055325+sumit0320@users.noreply.github.com>
2023-11-14 13:05:35 +0530

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# ## Some of the popular languages that Data Scientists use are:
+# 
+# 1. **Python:** Widely favored for its simplicity, extensive libraries (such as Pandas, NumPy, and Scikit-learn), and versatility in data analysis, machine learning, and visualization.
+# 
+# 2. **R:** Known for its statistical analysis capabilities and extensive packages like ggplot2, dplyr, and tidyr. It's commonly used in academia and research.
+# 
+# 3. **SQL:** Essential for working with databases, SQL (Structured Query Language) is used to retrieve, manipulate, and manage data stored in relational databases.
+# 
+# 4. **Julia:** Gaining traction due to its high-performance computing capabilities and syntax similar to Python and R. It's suitable for numerical and scientific computing.
+# 
+# 5. **Scala:** Popular for big data processing with frameworks like Apache Spark due to its scalability and compatibility with Java.
+# 
+# 6. **JavaScript (with libraries like TensorFlow.js):** For web-based visualizations, building interactive dashboards, and recently, for machine learning applications using TensorFlow.js.
+# 
+
+# ## Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. **Pandas:** A powerful library in Python for data manipulation and analysis, providing data structures and functions to work with structured data.
+# 
+# 2. **NumPy:** Fundamental package for scientific computing in Python, offering support for large, multi-dimensional arrays and matrices along with a collection of mathematical functions.
+# 
+# 3. **Scikit-learn:** An essential machine learning library in Python that provides simple and efficient tools for data mining, analysis, and building predictive models.
+# 
+# 4. **Matplotlib:** A widely used plotting library in Python for creating visualizations, histograms, scatter plots, etc., allowing data scientists to visualize their data and results.
+# 
+# 5. **TensorFlow:** An open-source machine learning framework developed by Google, useful for building and training neural network models.
+# 
+# 6. **Keras:** High-level neural networks API, often used in conjunction with TensorFlow, enabling fast experimentation and prototyping of deep learning models.
+# 
+# 7. **PyTorch:** Another popular open-source machine learning framework, known for its flexibility and ease of use, especially for building and training deep neural networks.
+# 
+# 8. **Seaborn:** Built on top of Matplotlib, Seaborn provides a high-level interface for drawing attractive and informative statistical graphics.
+# 
+# 9. **SciPy:** A collection of mathematical algorithms and convenience functions built on NumPy, offering additional functionalities for optimization, integration, linear algebra, etc.
+# 
+
+# | Data Science Tools   |
+# |----------------------|
+# | Jupyter Notebook     |
+# | RStudio              |
+# | Visual Studio Code   |
+# 
+
+# In[3]:
+
+
+### Below are a few examples of evaluating arithmetic expressions in Python
+
+
+result = 10 + 5
+print(result)
+result1 = 20 - 8
+print(result1) 
+result2 = 7 * 4
+print(result2)  
+result3 = 7 * 4
+print(result3)  
+result4 = 15 / 3
+print(result4)  
+result5 = 2 ** 3
+print(result5)  
+result6 = 17 % 5
+print(result6) 
+
+
+
+# In[4]:
+
+
+(3*4)+5 #This a simple arithmetic expression to mutiply then add integers
+
+
+# In[5]:
+
+
+minutes = 200
+
+# Conversion to hours
+hours = minutes / 60 #This will convert 200 minutes to hours by diving by 60
+print(f"{minutes} minutes is equal to {hours} hours")
+
+
+# **Objectives:**
+# 
+# - List popular languages used in Data Science.
+# - Explore commonly used libraries for Data Science tasks.
+# - Introduce tools and development environments for Data Science.
+# - Demonstrate arithmetic expression examples in Python.
+# - Convert minutes to hours using Python code.
+# 
+
+# ## Author
+# ChatGPT
+# 
+
+# In[ ]:
+
+
+
+
